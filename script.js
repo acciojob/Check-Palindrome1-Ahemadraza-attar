a22c4f163d77fe2a817f27055ea2308a61bb713c
Ahemadraza-attar <116665583+Ahemadraza-attar@users.noreply.github.com>
2023-06-14 15:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
-
+ let str2=str.split("").reverse().join("");
+	if (str2==str) 
+	{
+		console.log(true);
+	}
+	else
+	{
+		console.log(false);
+	}
 }
 module.exports = palindrome
